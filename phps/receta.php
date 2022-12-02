e2a8f3824f2265ec1663fbf002de992b3a1f1319
Yulian Pacheco <yulian1ro@gmail.com>
2022-12-02 15:01:01 -0600

    Receta.php

--- phps/receta.php
+++ phps/receta.php
@@ -1,149 +1,128 @@
-<?php 
+<?php
 
 require './db.php';
-  $id = $_GET['id'];
-  $recipe = $database->get("tb_recipes", [
-    "[>]tb_recipes_category" => ["recipe_category_id" => "id_recipe_category"], "[>]tb_recipes_complex" => ["recipe_complex_id" => "id_recipe_complex"]
-  ], [
-    "tb_recipes.id_recipe",
-    "tb_recipes.recipe_name",
-    "tb_recipes.recipe_time",
-    "tb_recipes.recipe_image",
-    "tb_recipes_category.recipe_category",
-    "tb_recipes_category.id_recipe_category",
-    'tb_recipes_complex.recipe_complex',
-    "tb_recipes.recipe_ingredients",
-    "tb_recipes.recipe_preparation",
-  
 
-  ], [
-    "tb_recipes.id_recipe" => $id,
-  ]);
+$id = $_GET['id'];
+$recipe = $database->get("tb_recipes", [
+  "[>]tb_recipes_category" => ["recipe_category_id" => "id_recipe_category"], "[>]tb_recipes_complex" => ["recipe_complex_id" => "id_recipe_complex"]
+], [
+  "tb_recipes.id_recipe",
+  "tb_recipes.recipe_name",
+  "tb_recipes.recipe_time",
+  "tb_recipes.recipe_time_preparation",
+  "tb_recipes.recipe_time_cooking",
+  "tb_recipes.recipe_image",
+  "tb_recipes.recipe_description",
+  "tb_recipes_category.recipe_category",
+  "tb_recipes_category.id_recipe_category",
+  'tb_recipes_complex.recipe_complex',
+  "tb_recipes.recipe_ingredients",
+  "tb_recipes.recipe_preparation",
+], [
+  "tb_recipes.id_recipe" => $id,
+]);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Receta</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/estilo.css">
-    <link rel="stylesheet" href="/css/utils.css">
-       
-    
+  <title>Receta</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/estilo.css">
+  <link rel="stylesheet" href="/css/utils.css">
+
+
 </head>
 
 <body>
 
- 
-    <!--Nav Bar-->
-    <?php include './header.php'; ?>
-        <!-- nav menu -->
-  
-    
-   
-    <!--Nav Bar-->
-    <!--Datos de la receta-->
-    <section class="container bg-green inner-bg-conf ma-5 pa-5">
-        <section class="row">
-            <section class="col-4 mr-5">            
-                <img src="/img/fotos/<?php 
-                  echo $recipe['recipe_image'];
-                ?>" class="img-fluid curved ml-4" alt=" <?php 
-                echo $recipe['recipe_name'];
-              ?>">
-            </section>      
-            <section class="col row">            
-                <h1 class="text-center"><?php 
-                  echo $recipe['recipe_name'];
-                ?></h1>
-                <section class="col">
-                    <p>Preparación: NA</p>
-                    <p>Cocción: NA</p>
-                    <p>Tiempo Total: <?php 
-                  echo $recipe['recipe_time'];
-                ?></p>
-                </section>
-                <section class="col pt-5">
-                    <p>Dificultad:  <?php 
-                  echo $recipe['recipe_complex'];
-                ?> </p>
-                </section>
-            </section>     
-        </section>
-    </section>
-    <!--Datos de la receta-->
 
-    <!--Descripción-->
-    <h2 class="text-center">Descripción</h2>
-    <section class="container bg-yellow inner-bg-conf ma-3 pa-3">
-        <p> <?php 
-                  echo $recipe['recipe_preparation'];
-                ?></p>
-    </section>
-    <!--Descripción-->
-    <!--Ingredientes y Preparación-->
-    <section class="container ma-5 pa-5">
-        <section class="row">
-            <section class="col bg-yellow inner-bg-conf ma-3 pa-3">
-                <h2 class="text-center">Ingredientes</h2>
-                <p> <?php 
-                  echo $recipe['recipe_ingredients'];
-                ?></p>
-            </section>
-            <section class="col bg-yellow inner-bg-conf ma-3 pa-3">
-                <h2 class="text-center">Preparación</h2>
-                <p> <?php 
-                  echo $recipe['recipe_preparation'];
-                ?></p>
-            </section>
+  <!--Nav Bar-->
+  <?php include './header.php'; ?>
+  <!-- nav menu -->
+
+
+
+  <!--Nav Bar-->
+  <!--Datos de la receta-->
+  <section class="container bg-green inner-bg-conf mt-5">
+    <section class="row">
+      <section class="col-4 m-5">
+        <img src="/img/fotos/<?php
+                              echo $recipe['recipe_image'];
+                              ?>" class="img-fluid curved ml-5" alt=" <?php
+                                                        echo $recipe['recipe_name'];
+                                                        ?>">
+      </section>
+      <section class="col row">
+        <h1 class="text-start-0 mt-4 pt-5"><?php
+                                echo $recipe['recipe_name'];
+                                ?></h1>
+        <section class="col">
+          <p>Tiempo de preparación: <?php
+                            echo $recipe['recipe_time_preparation'];
+                            ?></p>
+          <p>Tiempo de Cocción: <?php
+                            echo $recipe['recipe_time_cooking'];
+                            ?></p>
+          <p>Tiempo Total: <?php
+                            echo $recipe['recipe_time'];
+                            ?></p>
         </section>
+        <section class="col pt-5">
+          <p>Dificultad: <?php
+                          echo $recipe['recipe_complex'];
+                          ?> </p>
+        </section>
+      </section>
     </section>
-    <!--Ingredientes y Preparación-->
-    <!--Footer-->
-    <footer class="text-white">
-        <div class="container-fluid p-0">
-          <div class="row pie m-0">
-            <div class="col footer-img-container">
-              <img class="footer-img ml-1 mt-2" src="../img/logob.png" alt="">
-            </div>
-            <hr class="opacity-100 m-0 bg-white separador m-auto">
-            <div class="col d-flex flex-column align-items-start mb-2 px-4">
-              <h3>Siguenos en:</h3>
-              <div class="row gap-3 list-unstyled align-self-end">
-                <div class="col-1"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                    class="bi bi-facebook" viewBox="0 0 16 16">
-                    <path
-                      d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                  </svg></div>
-                <div class="col-1"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                    class="bi bi-twitter" viewBox="0 0 16 16">
-                    <path
-                      d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                  </svg></div>
-                <div class="col-1"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                    class="bi bi-instagram" viewBox="0 0 16 16">
-                    <path
-                      d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-                  </svg></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
- 
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous">
-      </script>
-   
-     
- 
-    <script src="/js/index.js"></script>
+  </section>
+  <!--Datos de la receta-->
+
+  <!--Descripción-->
+  
+  <section class="container bg-yellow inner-bg-conf mt-5">
+  <h2 class="text-center">Descripción</h2>
+    <p> <?php
+        echo $recipe['recipe_description'];
+        ?></p>
+  </section>
+  <!--Descripción-->
+  <!--Ingredientes y Preparación-->
+  <section class="container">
+    <section class="row">
+      <section class="col bg-yellow inner-bg-conf ma-3 pa-3">
+        <h2 class="text-center">Ingredientes</h2>
+        <p> <?php
+            echo $recipe['recipe_ingredients'];
+            ?></p>
+      </section>
+      <section class="col bg-yellow inner-bg-conf ma-3 pa-3">
+        <h2 class="text-center">Preparación</h2>
+        <p> <?php
+            echo $recipe['recipe_preparation'];
+            ?></p>
+      </section>
+    </section>
+  </section>
+  <!--Ingredientes y Preparación-->
+  <!--Footer-->
+  
+
+
+  
+
+  <?php 
+  include './footer.php';
+  ?>
+
+  <script src="/js/index.js"></script>
 
 </body>
+
 </html>
--- phps/update.php
+++ phps/update.php
@@ -1,5 +1,6 @@
 <?php
     require 'db.php';
+    
     function generateRandomString($length = 10) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
